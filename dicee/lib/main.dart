c36a14e00ac5c0e49983b69ae6d5338996eabd4c
Ale Mohamad <hello@alemohamad.com>
2019-10-17 01:36:17 +0200

    Centered Row and added buttons

--- dicee/lib/main.dart
+++ dicee/lib/main.dart
@@ -18,15 +18,27 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        Expanded(
-          child: Image.asset('images/dice1.png'),
-        ),
-        Expanded(
-          child: Image.asset('images/dice1.png'),
-        ),
-      ],
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('images/dice1.png'),
+              onPressed: () {
+                print('Left button was pressed');
+              },
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('images/dice1.png'),
+              onPressed: () {
+                print('Right button was pressed');
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
