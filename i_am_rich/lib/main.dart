08b3780e7f9135349046dca8cecae0826688b78a
Ale Mohamad <hello@alemohamad.com>
2019-10-16 11:40:03 +0200

    Add assets to the app

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -11,8 +11,7 @@ void main() {
         ),
         body: Center(
           child: Image(
-            image:
-                NetworkImage('https://www.w3schools.com/w3css/img_lights.jpg'),
+            image: AssetImage('images/diamond.png'),
           ),
         ),
       ),
