47e0b93811da87730d418710f53228bb6e486882
Ale Mohamad <hello@alemohamad.com>
2019-10-17 01:58:27 +0200

    Change StatelessWidget to StatefulWidget. Added math library, and get random values.

--- dicee/lib/main.dart
+++ dicee/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,7 +16,15 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -23,18 +32,22 @@ class DicePage extends StatelessWidget {
         children: <Widget>[
           Expanded(
             child: FlatButton(
-              child: Image.asset('images/dice1.png'),
               onPressed: () {
-                print('Left button was pressed');
+                setState(() {
+                  leftDiceNumber = Random().nextInt(6) + 1;
+                });
               },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
           ),
           Expanded(
             child: FlatButton(
-              child: Image.asset('images/dice1.png'),
               onPressed: () {
-                print('Right button was pressed');
+                setState(() {
+                  rightDiceNumber = Random().nextInt(6) + 1;
+                });
               },
+              child: Image.asset('images/dice$rightDiceNumber.png'),
             ),
           ),
         ],
