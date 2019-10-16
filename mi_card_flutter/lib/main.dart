e25fd3fd315e3311b25a2e9129fcc5c142585ce9
Ale Mohamad <hello@alemohamad.com>
2019-10-16 12:51:25 +0200

    Started working with Container

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -10,7 +10,19 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Container(
+            height: 100.0,
+            width: 100.0,
+//            margin: EdgeInsets.all(30.0),
+//            margin: EdgeInsets.symmetric(vertical: 50.0, horizontal: 15.0),
+//            margin: EdgeInsets.fromLTRB(30.0, 10.0, 50.0, 20.0),
+            margin: EdgeInsets.only(left: 30.0),
+            padding: EdgeInsets.all(20.0),
+            color: Colors.white,
+            child: Text('Hello'),
+          ),
+        ),
       ),
     );
   }
