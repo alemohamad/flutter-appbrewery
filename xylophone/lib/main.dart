a9e2caabccfcb8f53844223c681e4677d7bf4abc
Ale Mohamad <hello@alemohamad.com>
2019-10-17 10:40:26 +0200

    Separated buttons into a function, and finished the UI

--- xylophone/lib/main.dart
+++ xylophone/lib/main.dart
@@ -9,55 +9,33 @@ class XylophoneApp extends StatelessWidget {
     player.play('note$soundNumber.wav');
   }
 
+  Expanded buildKey({int position, Color color}) {
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        onPressed: () {
+          playSound(position);
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              FlatButton(
-                color: Colors.red,
-                onPressed: () {
-                  playSound(1);
-                },
-              ),
-              FlatButton(
-                color: Colors.orange,
-                onPressed: () {
-                  playSound(2);
-                },
-              ),
-              FlatButton(
-                color: Colors.yellow,
-                onPressed: () {
-                  playSound(3);
-                },
-              ),
-              FlatButton(
-                color: Colors.green,
-                onPressed: () {
-                  playSound(4);
-                },
-              ),
-              FlatButton(
-                color: Colors.teal,
-                onPressed: () {
-                  playSound(5);
-                },
-              ),
-              FlatButton(
-                color: Colors.blue,
-                onPressed: () {
-                  playSound(6);
-                },
-              ),
-              FlatButton(
-                color: Colors.purple,
-                onPressed: () {
-                  playSound(7);
-                },
-              ),
+              buildKey(position: 1, color: Colors.red),
+              buildKey(position: 2, color: Colors.orange),
+              buildKey(position: 3, color: Colors.yellow),
+              buildKey(position: 4, color: Colors.green),
+              buildKey(position: 5, color: Colors.teal),
+              buildKey(position: 6, color: Colors.blue),
+              buildKey(position: 7, color: Colors.purple),
             ],
           ),
         ),
