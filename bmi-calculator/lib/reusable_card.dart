24207ec314518b2ec5b36f1e536eb93b1a9873be
Ale Mohamad <hello@alemohamad.com>
2019-10-17 15:48:14 +0200

    Moved the Gesture Detector inside the widget, passing an anonymous function to the widget

--- bmi-calculator/lib/reusable_card.dart
+++ bmi-calculator/lib/reusable_card.dart
@@ -1,19 +1,23 @@
 import 'package:flutter/material.dart';
 
 class ReusableCard extends StatelessWidget {
-  ReusableCard({@required this.bgColor, this.cardChild});
+  ReusableCard({@required this.bgColor, this.cardChild, this.onPress});
 
   final Color bgColor;
   final Widget cardChild;
+  final Function onPress;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: cardChild,
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10.0),
-        color: bgColor,
+    return GestureDetector(
+      onTap: onPress,
+      child: Container(
+        child: cardChild,
+        margin: EdgeInsets.all(15.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+          color: bgColor,
+        ),
       ),
     );
   }
