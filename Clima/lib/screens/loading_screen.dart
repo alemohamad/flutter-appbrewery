d07a5368ff697dfe168a1a982b1c2b4fda97820d
Ale Mohamad <hello@alemohamad.com>
2019-10-18 11:37:20 +0200

    Added logic to get the network result, and show the location screen (with loading animation)

--- Clima/lib/screens/loading_screen.dart
+++ Clima/lib/screens/loading_screen.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+
+import 'package:clima/screens/location_screen.dart';
+
 import 'package:clima/services/location.dart';
 import 'package:clima/services/networking.dart';
+
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 const apiKey = 'eb2949be535ed7fa0f724ac251c6753b';
 
@@ -31,10 +36,28 @@ class _LoadingScreenState extends State<LoadingScreen> {
         'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
 
     var weatherData = await networkHelper.getData();
+
+    print(weatherData);
+
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) {
+          return LocationScreen();
+        },
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: SpinKitDoubleBounce(
+          color: Colors.white,
+          size: 100.0,
+        ),
+      ),
+    );
   }
 }
