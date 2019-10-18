caa424cbb14df602f11c91ae3b558f6e1672769f
Ale Mohamad <hello@alemohamad.com>
2019-10-18 12:31:48 +0200

    Connected the city screen, and added a custom text field

--- Clima/lib/screens/location_screen.dart
+++ Clima/lib/screens/location_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:clima/utilities/constants.dart';
 
+import 'package:clima/screens/city_screen.dart';
+
 import 'package:clima/services/weather.dart';
 
 class LocationScreen extends StatefulWidget {
@@ -80,7 +82,16 @@ class _LocationScreenState extends State<LocationScreen> {
                     ),
                   ),
                   FlatButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return CityScreen();
+                          },
+                        ),
+                      );
+                    },
                     child: Icon(
                       Icons.location_city,
                       size: 50.0,
